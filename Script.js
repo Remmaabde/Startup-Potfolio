319c1dd795a82e12b2ee1e13908f50a09d910645
Reema <Reemabdella702@gmail.com>
2025-06-08 22:26:50 +0300

    ADD Audience Toggle Switch

---
+++ Script.js
@@ -0,0 +1,40 @@
+//Our Current Slide
+let currentIndex = 0;
+
+function moveSlide(direction) {
+    const track = document.querySelector('.carousel-track');
+    const cards = document.querySelectorAll('.Product-card');
+    const totalSlides = cards.length;
+
+    // Changes z slide number depending on z button i clicked
+    currentIndex += direction;
+
+    //goes to prev slide 
+    if (currentIndex < 0) currentIndex = totalSlides - 1;
+    //When we reach the end of the slides, it goes back to the first slide
+    if (currentIndex >= totalSlides) currentIndex = 0;
+     
+    //checks how wide one product card is (in pixels)  //We need this number to know how far to move the slider.
+    const slideWidth = cards[0].clientWidth;
+
+    //move z entire carousel to left and right.
+
+    track.style.transform = `translateX(-${slideWidth * currentIndex}px)`;
+}
+function switchTo(target) {
+    // Hide both
+    document.getElementById('student').classList.remove('visible');
+    document.getElementById('mentor').classList.remove('visible');
+
+    // Show selected
+    document.getElementById(target).classList.add('visible');
+
+    // Button visual toggle
+    document.getElementById('studentBtn').classList.remove('active');
+    document.getElementById('mentorBtn').classList.remove('active');
+    document.getElementById(target + 'Btn').classList.add('active');
+}
+
+
+
+
